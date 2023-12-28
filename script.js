1ab1dfcc45dd16be82c9c834ce90745090adea2c
nabielmaulidina <n4b13l354@webmail.umm.ac.id>
2023-12-28 11:43:14 +0700

    UAP Pemrograman Web

---
+++ script.js
@@ -0,0 +1,12 @@
+var barang;
+document.getElementsByClassName(kode);
+
+let kode = 10000;
+let jumlah = 3;
+
+total = kode * jumlah;
+alert("total pembayaran adalah : " + total);
+prompt("masukkan uang user : ");
+
+kembalian = uang - total;
+alert("total kembalian anda adalah : " + kembalian);
